2313704282cb939106530e058c0cec0682c4efcd
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 20:13:26 +0530

    fix: changes of the instance

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 // https://www.freecodecamp.org/news/all-you-need-to-know-to-understand-javascripts-prototype-a2bff2d28f03/
 
 // Class syntax of es2015
-var foo = function (name) {
-    this.myName = name;
-    this.tellMyName = function () {
-        console.log(this.myName);
-    }
-}
+// var foo = function (name) {
+//     this.myName = name;
+//     this.tellMyName = function () {
+//         console.log(this.myName);
+//     }
+// }
+// since we create a instance of the object the tellMyName also taking memory space for each creations
+// so in order reduce to that
+var foo = function(name) { this.myName = name;}
 
 
 var obj1 = new foo('santhoshkumar');
