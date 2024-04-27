1b13a5933134eef766a50fc3df44d23115802d16
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 20:29:11 +0530

    fix: changes of the instance and comes under the same reference

--- script.js
+++ script.js
@@ -9,8 +9,12 @@
 // }
 // since we create a instance of the object the tellMyName also taking memory space for each creations
 // so in order reduce to that
-var foo = function(name) { this.myName = name;}
+var foo = function(name) { this.myName = name;};
 
+// dir use to list out of properties and methods
+console.dir(foo)
+
+// if we put under prototype object it will be available for multiple instance so they it will same reference
 foo.prototype.tellMyName = function() {   console.log(this.myName);}
 
 var obj1 = new foo('santhoshkumar');
@@ -18,4 +22,5 @@ console.log(obj1.tellMyName())
 var obj2 = new foo('sandy');
 console.log(obj2.tellMyName())
 
-
+// it because true based on the same reference 
+console.log(obj1.tellMyName === obj2.tellMyName)
