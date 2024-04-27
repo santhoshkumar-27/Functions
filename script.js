c1f572f4f7fef83105644d34f5be6de2b0f8fc16
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 19:52:27 +0530

    feat: before es 2022

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+// Class syntax of es2015
 var foo = function (name) {
     this.myName = name;
     this.tellMyName = function () {
         console.log(this.myName);
     }
 }
+
+
+var obj1 = new foo('santhoshkumar');
+console.log(obj1.tellMyName())
