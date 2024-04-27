e2f070351f02ef9b3d76e021ea66f3115153f304
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 20:08:53 +0530

    fix: immediate invoke function expression

--- fun.js
+++ fun.js
@@ -9,7 +9,14 @@ function name(parameter1, parameter2) {
     // the code statements to be executed
 }
 
-// anonyms functions
-const name = function (firstName) {
+// anonyms functions you should put a semi colan at the end of statement
+const na1me = function (firstName) {
     return 'Hello ' + firstName;
-}
+};
+// this is like a expressions
+
+
+
+(function () {
+    console.log('immediately invoked funtions');
+})()
