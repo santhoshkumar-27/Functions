b61118f2562d39fa5919ec79a7562c713b13dae7
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 19:59:36 +0530

    fix: anonyms functions

--- fun.js
+++ fun.js
@@ -8,3 +8,8 @@ greeting();
 function name(parameter1, parameter2) {
     // the code statements to be executed
 }
+
+// anonyms functions
+const name = function (firstName) {
+    return 'Hello ' + firstName;
+}
