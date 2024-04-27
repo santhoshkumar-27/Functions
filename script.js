b13084747c6a5d997a515f3a557dc232abdbd2bf
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-27 20:11:09 +0530

    fix: changes scripts

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// https://www.freecodecamp.org/news/all-you-need-to-know-to-understand-javascripts-prototype-a2bff2d28f03/
+
 // Class syntax of es2015
 var foo = function (name) {
     this.myName = name;
@@ -9,3 +11,5 @@ var foo = function (name) {
 
 var obj1 = new foo('santhoshkumar');
 console.log(obj1.tellMyName())
+
+
