ddde3d2a79457135dbb8069f40f81314ef31d44a
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-28 14:13:29 +0530

    fix: accelerator

--- protoChaining.js
+++ protoChaining.js
@@ -6,3 +6,27 @@ function Vehicle(vehicleType) {
 Vehicle.prototype.horn = function () {
     console.log('honk honk honk')
 }
+
+function Bus(make) {
+    // Bus Constructor  
+    // inheritance of Vehicle
+    Vehicle.call(this, "Bus");
+    this.make = make
+}
+
+// Object.create method create prototype to the object from the mention object
+Bus.prototype = Object.create(Vehicle.prototype)
+
+Bus.prototype.noOfWheels = 6; // Let's assume all buses have 6 wheels
+
+Bus.prototype.accelerator = function () {
+    console.log('Accelerating Bus');
+    //Bus accelerator
+}
+
+Bus.prototype.brake = function () {
+    console.log('Braking Bus');
+    // Bus brake
+}
+
+console.dir(Bus)
