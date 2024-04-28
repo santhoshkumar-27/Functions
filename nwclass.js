dd0b691dbf93e143e64251bccc6b2ed5ba339fd4
santhoshkumar <its.santhoshkumar@outlook.com>
2024-04-28 17:22:13 +0530

    static method

--- nwclass.js
+++ nwclass.js
@@ -12,3 +12,19 @@ const myObj = new Myclass("John");
 
 
 console.log('this.tellMyName', myObj)
+// creating static method with new es2015 syntax
+class Myclass {
+    static welcome() {
+        console.log("Hello World");
+    }
+}
+
+/**
+ * Static properties are something that
+ * you can access without creating an instance of the class. 
+ * On the other hand, the instance will not have access to the static properties of a class.
+*/
+Myclass.welcome()
+
+const myObj1 = new Myclass();
+myObj1.welcome();
