658772bb327d3d903d161aa718bcb4f841080977
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-04 12:21:48 +0530

    feat: context of bind is completed

--- callapplybind.js
+++ callapplybind.js
@@ -49,7 +49,7 @@ console.log('newBrand1', newBrand)
 
 
 function addUp() {
-    console.log('arguments',arguments)
+    console.log('arguments', arguments)
     //Using arguments to capture the arbitrary number of inputs
     const args = Array.from(arguments);
     this.x = args.reduce((prev, curr) => prev + curr, 0);
@@ -64,3 +64,41 @@ function driverFunc() {
 }
 
 driverFunc();
+
+// Bind method or Hard binding
+const car = {
+    brand: 'Lamborghini',
+};
+
+// As of now, 'this' keyword refers
+// to 'window' object.
+const printDetail = function (model, topSpeed) {
+    console.log(`${this.brand} ${model} has a 
+    top speed of ${topSpeed} mph`);
+};
+
+// Calling the function without using bind which
+// means 'this' still refers to 'window' object
+// so accessing this.brand will give undefined
+
+// printDetail('Diablo Coatl', 239);
+
+
+// Creating a new function based on printDetail
+// with 'this' keyword referring to car object
+// so accessing this.brand will give 'Lamborgini'
+const lamboPrintDetail = printDetail.bind(car);
+// console.dir(lamboPrintDetail)
+lamboPrintDetail('Diablo VTTT', 222);
+
+
+// Creating another function based on printDetail
+// with it's arguments pre-configured and 'this'
+// keyword referring to car object
+const reventonPrintDetail
+    = printDetail.bind(car, 'Reventon', 221);
+console.dir(reventonPrintDetail)
+
+// Since the arguments are preconfigured so we don't
+// need to pass any argument to call this function
+reventonPrintDetail();
