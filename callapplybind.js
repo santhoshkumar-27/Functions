020f440e467a22abf39c369a3fa687ad451e5caf
santhoshkumar <its.santhoshkumar@outlook.com>
2024-05-04 09:35:18 +0530

    feat: Using call method we implement inheritence

--- callapplybind.js
+++ callapplybind.js
@@ -1,1 +1,18 @@
 // https://www.freecodecamp.org/news/understand-call-apply-and-bind-in-javascript-with-examples/#prerequisites
+
+function Car(type, fuelType) {
+    this.type = type;
+    this.fuelType = fuelType;
+}
+
+function setBrand(brand, price) {
+    Car.call(this, "convertible", "petrol");
+    this.brand = brand;
+    this.price = price;
+    console.log(`Car details = `, this);
+}
+
+const newBrand = new setBrand('Brand1', 100000);
+
+
+console.log('newBrand', newBrand)
